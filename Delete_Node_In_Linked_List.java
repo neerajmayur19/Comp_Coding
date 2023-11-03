de0434b485ce468d6f575195a2ecb700a98159a3
Neeraj Mayur <neeraj.mayur.999@gmail.com>
2023-11-03 22:12:45 +0530

    Create Delete_Node_In_Linked_List.java
    The Approach is pretty simple, wherein you are not exactly deleting the node, but just the value. The Node is given the value of the next node's value and the node's next becomes the node's next's next.
    So all constraints are satisfied then.
    TC:O(1)
    SC:O(1)

---
+++ Delete_Node_In_Linked_List.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        int value = node.next.val;
+        node.val = value;
+        node.next = node.next.next;
+    }
+}

070839aee2f7c9a6cd0aa0a85cd67e4a14f75b86
Neeraj Mayur <neeraj.mayur.999@gmail.com>
2024-03-10 23:01:14 +0530

    Create Minimum_Platforms.java
    Employs Greedy Method to solve it. Sub-topics Include Sorting and Two-Pointer Approach.
    TC:O(N log N) + O(N)
    SC:O(1)

---
+++ Minimum_Platforms.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            
+            int arr[] = new int[n];
+            int dep[] = new int[n];
+            
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(str[i]);
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                dep[i] = Integer.parseInt(str[i]);
+                
+            System.out.println(new Solution().findPlatform(arr, dep, n));
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int maximum = 1;
+        int final_ans = 1;
+        int i=1,j=0;
+        while(i<arr.length && j<dep.length)
+        {
+            if(arr[i]<=dep[j])
+            {
+                maximum++;
+                i++;
+            }
+            else if(arr[i]>dep[j])
+            {
+                maximum--;
+                j++;
+            }
+            final_ans = Math.max(final_ans,maximum);
+        }
+        return final_ans;
+        
+    }
+    
+}
+
